ebca7b9c63af4fd0ca3a4651c6dc4b723c4ff78a
devsandrin <a.silvanj@hotmail.com>
2024-06-02 14:03:32 -0300

    Criando a recyclerView main e a lógica do carrossel de filmes

--- app/src/main/java/co/tiagoaguiar/netflixremake/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/netflixremake/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import co.tiagoaguiar.netflixremake.model.Category
 import co.tiagoaguiar.netflixremake.model.Movie
 
 class MainActivity : AppCompatActivity() {
@@ -12,15 +13,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val movies = mutableListOf<Movie>()
-        for(i in 0 until 5) {
-            val movie = Movie(R.drawable.movie)
-            movies.add(movie)
+
+        val categories = mutableListOf<Category>()
+        for (j in 0 until 5) {
+
+            val movies = mutableListOf<Movie>()
+            for(i in 0 until 15) {
+                val movie = Movie(R.drawable.movie)
+                movies.add(movie)
+            }
+
+            val category = Category("cat $j", movies)
+            categories.add(category)
         }
 
-        val adapter = MainAdapter(movies)
+
+
+        val adapter = CategoryAdapter(categories)
         val rv: RecyclerView = findViewById(R.id.rv_main)
-        rv.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+        rv.layoutManager = LinearLayoutManager(this)
         rv.adapter = adapter
 
     }
