47e2affd9cf8f0b0c6b3f0618cd60cf22e1b0eb8
devsandrin <a.silvanj@hotmail.com>
2024-06-03 21:54:31 -0300

    Criando barra de ação: Toolbar

---
+++ app/src/main/java/co/tiagoaguiar/netflixremake/MovieActivity.kt
@@ -0,0 +1,22 @@
+package co.tiagoaguiar.netflixremake
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+
+class MovieActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie)
+
+        val toolbar: Toolbar = findViewById(R.id.movie_toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.baseline_arrow_back_24)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+}
