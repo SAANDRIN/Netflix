90ed0d3cac1e2c413d12946062d8f53047e9bbfb
devsandrin <a.silvanj@hotmail.com>
2024-06-11 20:38:08 -0300

    Sincronizando ScrollView com RecyclerView (Envelopando RecyclerView)

--- app/src/main/java/co/tiagoaguiar/netflixremake/MovieActivity.kt
+++ app/src/main/java/co/tiagoaguiar/netflixremake/MovieActivity.kt
@@ -3,9 +3,13 @@ package co.tiagoaguiar.netflixremake
 import android.graphics.drawable.LayerDrawable
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import co.tiagoaguiar.netflixremake.model.Movie
 
 
 class MovieActivity : AppCompatActivity() {
@@ -15,11 +19,32 @@ class MovieActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie)
 
+        val txtTitle: TextView = findViewById(R.id.movie_txt_title)
+        val txtDesc: TextView = findViewById(R.id.movie_txt_desc)
+        val txtCast: TextView = findViewById(R.id.movie_txt_cast)
+        val rv: RecyclerView = findViewById(R.id.movie_rv_similar)
+
+        txtTitle.text = "Batman Begins"
+        txtDesc.text = "Essa é a descrição do filme do Batman"
+        txtCast.text = getString(R.string.cast, "Ator A, Ator B, Atriz A, Atriz B")
+
+        val movies = mutableListOf<Movie>()
+        for(i in 0 until 15) {
+            val movie = Movie(R.drawable.movie)
+            movies.add(movie)
+        }
+
+        rv.layoutManager = GridLayoutManager(this, 3)
+        rv.adapter = MovieAdapter(movies, R.layout.movie_item_similar)
+
+
+
         val toolbar: Toolbar = findViewById(R.id.movie_toolbar)
         setSupportActionBar(toolbar)
 
         supportActionBar?.setHomeAsUpIndicator(R.drawable.baseline_arrow_back_24)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = null
 
 
         val layerDrawable: LayerDrawable = ContextCompat.getDrawable(this, R.drawable.shadows) as LayerDrawable
